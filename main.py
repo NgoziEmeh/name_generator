aefcc7ab888cf10c2ba4b43f9dcd134340ddeeb7
NgoziEmeh <zingo1ng@yahoo.com>
2021-10-04 05:21:06 -0700

    team value

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ print("Welcome to Ngozi's Team Name Generator")
 
 city_name = input("What is the name of your city?\n")
 
-pet_name = input("What is the name of your pet?\n")
+team_value = input("What is the word that represents the value you want to see in your team? E.g:United,Warriors,Rangers,etc.\n")
 
-print(f"Suggested name for your team is {city_name} {pet_name}")
+print(f"Suggested name for your team is {city_name} {team_value}")
 
 
 
