56d1e4bf1c34b56c767e0ac867396e697d85b649
NgoziEmeh <zingo1ng@yahoo.com>
2021-10-04 05:11:04 -0700

    line break

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 print("Welcome to Ngozi's Team Name Generator")
 
-city_name = input("What is the name of your city?")
-print(city_name)
+city_name = input("What is the name of your city?\n")
 
-pet_name = input("What is the name of your pet?")
+pet_name = input("What is the name of your pet?\n")
 
 print(f"Suggested name for your team is {city_name} {pet_name}")
 
